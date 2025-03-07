62e2e57d7a7c89053b3d6cc5675e51018931bd66
Roman Penyaev <r.peniaev@gmail.com>
2025-03-07 13:23:49 +0100

    diablo: add `setlinebuf` for stdout
    diablo_state.py collects stdout prints and redirects them to stdout
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- Source/diablo.cpp
+++ Source/diablo.cpp
@@ -2597,6 +2597,8 @@ void setOnInitialized(void (*callback)())
 
 int DiabloMain(int argc, char **argv)
 {
+	setlinebuf(stdout);
+
 #ifdef _DEBUG
 	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
 #endif
