wkdgmrclub/devilutionX

commit 88d0cb749fdcd6c5b4fd706f57d355a876d6a5e3

lighting: fix long-standing issue with invisible objects (#7901)
* lighting: fix long-standing issue with invisible objects

This fixes an issue with lighting, where objects in a straight line of
sight parallel to the X or Y coordinate lines become invisible. Issue
 #6641 perfectly illustrates the bug (see video attached to the bug).

What's worse, the objects are invisible to the observer (player)
regardless of the distance to that objects. The main requirement of a
bug reproduction is line of sight parallel to the X or Y coordinate
lines.

The actual bug lies in the visibility checks of adjacent tiles of a
point, hit by the cast ray. We've cast an approximated ray on an
integer 2D grid, so we need to check if a ray can pass through the
diagonally adjacent tiles. For example, consider this case:

       #?
      ↗ #
    x

The ray is cast from the observer 'x', and reaches the '?', but
diagonally adjacent tiles '#' do not pass the light, so the '?' should
not be visible for the 2D observer.

The trick is to perform two additional visibility checks for the
diagonally adjacent tiles, but only for the rays that are not parallel
to the X or Y coordinate lines. Parallel rays, which have a 0 in one
of their coordinate components, do not require any additional adjacent
visibility checks, and the tile, hit by the ray, is always considered
visible.

For the rays that parallel to the X or Y coordinate lines, the adjacent
visibility check always degenerated to the actual ray point visibility
check, which is considered invisible if it does not allow light to
pass through, and this is the actual bug.

To fix the issue, ensure the tile is always considered visible if the
ray that hits it is parallel to the X or Y coordinate lines.

To better demonstrate the problem, here's a straightforward simulation
written in Python:

   https://gist.github.com/rouming/25c555720f93735442c2053426e73bf5

The code simulates lighting from the DevilitionX implementation, by

Source/lighting.cpp
@@ -41,10 +41,15 @@ bool UpdateLighting;
 namespace {
 
 /*
- * X- Y-coordinate offsets of lighting visions.
- * The last entry-pair is only for alignment.
+ * XY points of vision rays are cast to trace the visibility of the
+ * surrounding environment. The table represents N rays of M points in
+ * one quadrant (0°-90°) of a circle, so rays for other quadrants will
+ * be created by mirroring. Zero points at the end will be trimmed and
+ * ignored. A similar table can be recreated using Bresenham's line
+ * drawing algorithm, which is suitable for integer arithmetic:
+ * https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
  */
-const DisplacementOf<int8_t> VisionCrawlTable[23][15] = {
+static const DisplacementOf<int8_t> VisionRays[23][15] = {
 	// clang-format off
 	{ { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }, { 7, 0 }, { 8, 0 }, { 9, 0 }, { 10,  0 }, { 11,  0 }, { 12,  0 }, { 13,  0 }, { 14,  0 }, { 15,  0 } },
 	{ { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }, { 7, 0 }, { 8, 1 }, { 9, 1 }, { 10,  1 }, { 11,  1 }, { 12,  1 }, { 13,  1 }, { 14,  1 }, { 15,  1 } },
@@ -79,9 +84,6 @@ uint8_t LightFalloffs[NumLightRadiuses][128];
 bool UpdateVision;
 /** interpolations of a 32x32 (16x16 mirrored) light circle moving between tiles in steps of 1/8 of a tile */
 uint8_t LightConeInterpolations[8][8][16][16];
-
-/** RadiusAdj maps from VisionCrawlTable index to lighting vision radius adjustment. */
-const uint8_t RadiusAdj[23] = { 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 4, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0 };
 
 void RotateRadius(DisplacementOf<int8_t> &offset, DisplacementOf<int8_t> &dist, DisplacementOf<int8_t> &light, DisplacementOf<int8_t> &block)
 {
@@ -232,31 +234,67 @@ void DoVision(Point position, uint8_t radius, MapExplorationType doAutomap, bool
 {
 	DoVisionFlags(position, doAutomap, visible);
 
-	static const Displacement factors[] = { { 1, 1 }, { -1, 1 }, { 1, -1 }, { -1, -1 } };
-	for (auto factor : factors) {
-		for (int j = 0; j < 23; j++) {
-			int lineLen = radius - RadiusAdj[j];
-			for (int k = 0; k < lineLen; k++) {
-				Point crawl = position + VisionCrawlTable[j][k] * factor;
-				if (!InDungeonBounds(crawl))
-					break;
-				bool blockerFlag = TileHasAny(crawl, TileProperties::BlockLight);
-				bool tileOK = !blockerFlag;
+	// Adjustment to a ray length to ensure all rays lie on an
+	// accurate circle
+	static const uint8_t rayLenAdj[23] = { 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 4, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0, 0 };
+	static_assert(std::size(rayLenAdj) == std::size(VisionRays));
 
-				if (VisionCrawlTable[j][k].deltaX > 0 && VisionCrawlTable[j][k].deltaY > 0) {
-					tileOK = tileOK || TileAllowsLight(crawl + Displacement { -factor.deltaX, 0 });
-					tileOK = tileOK || TileAllowsLight(crawl + Displacement { 0, -factor.deltaY });
+	// Four quadrants on a circle
+	static const Displacement quadrants[] = { { 1, 1 }, { -1, 1 }, { 1, -1 }, { -1, -1 } };
+
+	// Loop over quadrants and mirror rays for each one
+	for (const auto &quadrant : quadrants) {
+		// Cast a ray for a quadrant
+		for (unsigned int j = 0; j < std::size(VisionRays); j++) {
+			int rayLen = radius - rayLenAdj[j];
+			for (int k = 0; k < rayLen; k++) {
+				const auto &relRayPoint = VisionRays[j][k];
+				// Calculate the next point on a ray in the quadrant
+				Point rayPoint = position + relRayPoint * quadrant;
+				if (!InDungeonBounds(rayPoint))
+					break;
+
+				bool visible = true;
+
+				//
+				// We've cast an approximated ray on an integer 2D
+				// grid, so we need to check if a ray can pass through
+				// the diagonally adjacent tiles. For example, consider
+				// this case:
+				//
+				//        #?
+				//       ↗ #
+				//     x
+				//
+				// The ray is cast from the observer 'x', and reaches
+				// the '?', but diagonally adjacent tiles '#' do not
+				// pass the light, so the '?' should not be visible
+				// for the 2D observer.
+				//
+				// The trick is to perform two additional visibility
+				// checks for the diagonally adjacent tiles, but only
+				// for the rays that are not parallel to the X or Y
+				// coordinate lines. Parallel rays, which have a 0 in
+				// one of their coordinate components, do not require
+				// any additional adjacent visibility checks, and the
+				// tile, hit by the ray, is always considered visible.
+				//
+				if (relRayPoint.deltaX > 0 && relRayPoint.deltaY > 0) {
+					Displacement adjacent1 = { -quadrant.deltaX, 0 };
+					Displacement adjacent2 = { 0, -quadrant.deltaY };
+
+					visible = (TileAllowsLight(rayPoint + adjacent1) || TileAllowsLight(rayPoint + adjacent2));
 				}
+				if (visible)
+					DoVisionFlags(rayPoint, doAutomap, visible);
 
-				if (!tileOK)
+				bool passesLight = TileAllowsLight(rayPoint);
+				if (!passesLight)
+					// Tile does not pass the light further, we are
+					// done with this ray
 					break;
 
-				DoVisionFlags(crawl, doAutomap, visible);
-
-				if (blockerFlag)
-					break;
-
-				int8_t trans = dTransVal[crawl.x][crawl.y];
+				int8_t trans = dTransVal[rayPoint.x][rayPoint.y];
 				if (trans != 0)
 					TransList[trans] = true;
 			}

rouming/devilutionX-AI

commit 3491ab266b364f52c972f98cf99fabf87fc2eb66

lighting: fix vision through diagonally adjacent tiles
This commit fixes a very subtle case where players could see through
diagonally adjacent tiles. The bug was introduced in the recent
commit: 88d0cb749fdc ("lighting: fix long-standing issue with
invisible objects (#7901)").

The following 2D grid illustrates the bug:

```
  . .                                       . .
  . . .                                   . . .
    . . .                               . . .
      . . .                           . . .
        . . .                       . . .
          . . .                   . . .
            . .                   . .
                  # # # # # # #
                # . . . . . . . #
                # . . . . . . . #
                # . . . . . . . #
                # . . . x . . . #
                # . . . . . . . #
                # . . . . . . . #
                # . . . . . . . #
                  # # # # # # #
            . .                   . .
          . . .                   . . .
        . . .                       . . .
      . . .                           . . .
    . . .                               . . .
  . . .                                   . . .
  . .                                       . .
```

Where 'x' represents the player, surrounded by walls with diagonally
adjacent corners, and '.' represents visible tiles reached by vision
rays cast from the player's position. The figure clearly shows that
rays "leak" through corners.

The fix is quite simple: stop traversing the ray if "light" can't pass
through the adjacent tiles or if the ray hits a tile that doesn't
allow "light" to pass through. Previously, the ray continued to be
traversed even when the "light" couldn't pass through the adjacent
tiles, which is incorrect and leads to the described issue.

Here is the Python script which simulates the bug and the fix:

  https://gist.github.com/rouming/8a24789fd5d18c36c40e2b4925915d16

Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

Source/lighting.cpp
@@ -254,9 +254,6 @@ void DoVision(Point position, uint8_t radius, MapExplorationType doAutomap, bool
 				if (!InDungeonBounds(rayPoint))
 					break;
 
-				bool visible = true;
-
-				//
 				// We've cast an approximated ray on an integer 2D
 				// grid, so we need to check if a ray can pass through
 				// the diagonally adjacent tiles. For example, consider
@@ -283,10 +280,13 @@ void DoVision(Point position, uint8_t radius, MapExplorationType doAutomap, bool
 					Displacement adjacent1 = { -quadrant.deltaX, 0 };
 					Displacement adjacent2 = { 0, -quadrant.deltaY };
 
-					visible = (TileAllowsLight(rayPoint + adjacent1) || TileAllowsLight(rayPoint + adjacent2));
+					bool passesLight = (TileAllowsLight(rayPoint + adjacent1) || TileAllowsLight(rayPoint + adjacent2));
+					if (!passesLight)
+						// Diagonally adjacent tiles do not pass the
+						// light further, we are done with this ray
+						break;
 				}
-				if (visible)
-					DoVisionFlags(rayPoint, doAutomap, visible);
+				DoVisionFlags(rayPoint, doAutomap, visible);
 
 				bool passesLight = TileAllowsLight(rayPoint);
 				if (!passesLight)